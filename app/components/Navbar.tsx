8c2c548134fdad0676729be204f0ba02dca162d1
jovanpet <pjovan65@gmail.com>
2024-07-15 19:58:59 -0400

    Formatting

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -8,33 +8,33 @@ import { useState } from 'react';
  * @returns {JSX.Element} The Navbar Component for the application
  */
 const Navbar = () => {
-  const [search, setSearch] = useState<string>('');
+    const [search, setSearch] = useState<string>('');
 
-  /**
-   * Handles the search form submission
-   *
-   * @param e The event object from the input form
-   */
-  const handleSearch = (e: React.FormEvent) => {
-    e.preventDefault();
-    console.log(search);
-  };
+    /**
+     * Handles the search form submission
+     *
+     * @param e The event object from the input form
+     */
+    const handleSearch = (e: React.FormEvent) => {
+        e.preventDefault();
+        console.log(search);
+    };
 
-  return (
-    <nav className="bg-white text-black w-full flex justify-between p-2">
-      <p>Logo</p>
-      <form onSubmit={handleSearch} className="w-1/2">
-        <input 
-          className="w-full border-2 border-black rounded-2xl p-1"
-          type="text"
-          value={search}
-          onChange={(e) => setSearch(e.target.value)}
-          placeholder="Search..."
-        />
-      </form>
-      <p>Profile Icon</p>
-    </nav>
-  );
+    return (
+        <nav className="bg-white text-black w-full flex justify-between p-2">
+            <p>Logo</p>
+            <form onSubmit={handleSearch} className="w-1/2">
+                <input
+                    className="w-full border-2 border-black rounded-2xl p-1"
+                    type="text"
+                    value={search}
+                    onChange={(e) => setSearch(e.target.value)}
+                    placeholder="Search..."
+                />
+            </form>
+            <p>Profile Icon</p>
+        </nav>
+    );
 };
 
 export default Navbar;
