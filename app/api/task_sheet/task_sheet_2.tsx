87ae7694caf5a85c5a05631da10630e6b3dd8e06
johan253 <johannjo2000@gmail.com>
2024-07-31 15:38:14 -0700

    Modified endpoint to use NextRequest and NextResponse objects

--- app/api/task_sheet/task_sheet_2.tsx
+++ app/api/task_sheet/task_sheet_2.tsx
@@ -1,4 +1,5 @@
 import { TaskSheet } from '../interfaces/interfaces';
+import { NextResponse, NextRequest } from 'next/server';
 
 // This constant is for testing purposes only, remove when not needed
 const taskSheets: TaskSheet[] = [
@@ -35,8 +36,21 @@ const taskSheets: TaskSheet[] = [
  * @param id The ID of the task sheet to get
  * @returns The task sheet with the given ID
  */
-const getTaskSheetById = (id: number): TaskSheet | undefined => {
-    return taskSheets.find(taskSheet => taskSheet.id === id);
+const getTaskSheetById = async(req: NextRequest) => {
+    const id: string = req.nextUrl.searchParams.get("Id") || "";
+    if (!id) {
+        return NextResponse.json({ error: 'ID parameter is required' }, { status: 400 });
+    }
+    if (Array.isArray(id)) {
+        return NextResponse.json({ error: 'ID must be a single value' }, { status: 400 });
+    }
+    const num_id = parseInt(id);
+
+    let taskSheet = taskSheets.find(ts => ts.id === num_id);
+    if (taskSheet === undefined) {
+        return NextResponse.json({ error: 'Task sheet with that ID does not exist' }, { status: 404 });
+    }
+    return NextResponse.json({ body: taskSheet }, { status: 200 });
 };
 
 export default getTaskSheetById;
