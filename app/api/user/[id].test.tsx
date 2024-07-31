1f732e8de8234af5d152217cf19c69f22637f72d
jovanpet <pjovan65@gmail.com>
2024-07-31 17:03:23 -0400

    Jovan Fixes moving away from this PR

--- app/api/user/[id].test.tsx
+++ app/api/user/[id].test.tsx
@@ -1,10 +1,10 @@
 /**
  * @jest-environment node
  */
-import {GetUserTimeSheets} from './[id].tsx';
+import { GetUserTimeSheets } from './[id].tsx';
 
 
-const expected_user_json =  "{\"user\":{\"id\":2,\"task_sheet_ids\":\"[{ \\\"id\\\": 1 , tasks: \\\"[{task_id:1,task_name:\\\"homework1\\\",completed:false,user_ids:\\\"[{\\\\\\\"id\\\\\\\":1},\\\"+\\\"{\\\\\\\"id\\\\\\\":2}]\\\"},{task_id:2,task_name:\\\"homework2\\\",completed:true,user_ids:\\\"[{\\\\\\\"id\\\\\\\":1},\\\"+\\\"{\\\\\\\"id\\\\\\\":2}]\\\"}]\\\"},{ \\\"id\\\":2 , tasks: \\\"[{task_id:1,task_name:\\\"homework1\\\",completed:false,user_ids:\\\"[{\\\\\\\"id\\\\\\\":1},\\\"+\\\"{\\\\\\\"id\\\\\\\":2}]\\\"},{task_id:2,task_name:\\\"homework2\\\",completed:true,user_ids:\\\"[{\\\\\\\"id\\\\\\\":1},\\\"+\\\"{\\\\\\\"id\\\\\\\":2}]\\\"}]\\\"} ]\",\"first_name\":\"Patrick\",\"second_name\":\"Star\",\"student\":true}}";
+const expected_user_json = "{\"body\":{\"task_sheet_ids\":[{\"id\":1},{\"id\":2}]}}"
 it('should return data with status 200', async () => {
 
     const requestObj = {
@@ -34,5 +34,3 @@ it('should return error with status 400 when item not found', async () => {
     const body = await response.json();
     expect(body).toEqual({ error: 'User with that ID does not exist' });
 });
-
-//TODO add tests checking all the 400 errors
--- app/api/user/[id].tsx
+++ app/api/user/[id].tsx
@@ -1,19 +1,16 @@
 import { NextResponse, NextRequest } from 'next/server';
-import {User} from '../interfaces/interfaces.tsx';
+import { User } from '../interfaces/interfaces.tsx';
 const task_sheet_json =
-    '[' +
-    '{ "id": 1 , tasks: "[{task_id:1,task_name:\"homework1\",completed:false,user_ids:\"[{\\\"id\\\":1},\"+\"{\\\"id\\\":2}]\"},{task_id:2,task_name:\"homework2\",completed:true,user_ids:\"[{\\\"id\\\":1},\"+\"{\\\"id\\\":2}]\"}]"},' +
-    '{ "id":2 , tasks: "[{task_id:1,task_name:\"homework1\",completed:false,user_ids:\"[{\\\"id\\\":1},\"+\"{\\\"id\\\":2}]\"},{task_id:2,task_name:\"homework2\",completed:true,user_ids:\"[{\\\"id\\\":1},\"+\"{\\\"id\\\":2}]\"}]"} ]';
-
+    '{ "task_sheet_ids" : [' +
+    '{ "id": 1 },' +
+    '{ "id":2 } ]}';
 const users: User[] = [
-    { "id": 1, "task_sheet_ids": task_sheet_json, "first_name": "Sponge", "second_name": "Bob", "student": false },
-    { "id": 2, "task_sheet_ids": task_sheet_json, "first_name": "Patrick", "second_name": "Star", "student": true }
+    { "id": 1, "task_sheets": JSON.parse(task_sheet_json), "first_name": "Sponge", "second_name": "Bob", "student": false },
+    { "id": 2, "task_sheets": JSON.parse(task_sheet_json), "first_name": "Patrick", "second_name": "Star", "student": true }
 ];
 
-
-
 export async function GetUserTimeSheets(req: NextRequest) {
-    const id:string= req.nextUrl.searchParams.get("Id") || "";
+    const id: string = req.nextUrl.searchParams.get("Id") || "";
     if (!id) {
         return NextResponse.json({ error: 'id is required' }, { status: 400 });
     }
@@ -21,13 +18,11 @@ export async function GetUserTimeSheets(req: NextRequest) {
         return NextResponse.json({ error: 'name must be a single value' }, { status: 400 });
     }
     const num_id = parseInt(id);
-    //let taskSheets = findUsersTaskSheets(intID)
-    //get user from sample array by index
 
-    //probably should be num_id-1 because the users start with 1 and array starts with 0
     let user = users[num_id];
-    if (user=== undefined) {
+    if (user === undefined) {
         return NextResponse.json({ error: 'User with that ID does not exist' }, { status: 404 });
     }
-    return NextResponse.json({ user }, { status: 200 });
+    let body = user.task_sheets;
+    return NextResponse.json({ body }, { status: 200 });
 }
